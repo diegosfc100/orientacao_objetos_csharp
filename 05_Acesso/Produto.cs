be33849c5380e4882b60c4d39585db88da33b7dd
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-30 20:48:45 -0300

    Modificadores de Acesso

---
+++ 05_Acesso/Produto.cs
@@ -0,0 +1,14 @@
+class Produto
+{
+    // Modificadores de Acesso, é a maneira q vc tem de visualizar um atributo ou método;
+        // public    ~> atributos e métodos visíveis em qlqr classe.
+        // private   ~> atributos e métodos visíveis apenas na classe onde são ciados.
+        // protected ~> atributos e métodos visíveis em classes onde são criados ou herdados.
+
+
+    public string nome;
+
+    private double peso;
+
+    protected string sobrenome;
+}
---
+++ 05_Acesso/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Produto obj = new Produto();
+        obj.nome = "Diego"; // Apenas o atributo 'nome' pode ser instanciado devido seu acesso ser public.
+    }
+}
