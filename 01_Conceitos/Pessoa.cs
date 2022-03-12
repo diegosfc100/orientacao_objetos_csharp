9cf24a9fb08112e71a11dcca22382a0cde88ff7f
diegosfc100 <diego.sfc100@hotmail.com>
2022-03-11 22:42:02 -0300

    Update

--- 01_Conceitos/Pessoa.cs
+++ 01_Conceitos/Pessoa.cs
@@ -1,19 +1,24 @@
-using System;
+using System; // using é utilizado para importar funcionalidades; System é a biblioteca padrão.
 
-// CLASSE: É como se fosse um molde. (Ex: Pessoa)
-// ATRIBUTOS: Caracteríscias. (Ex: nome, idade, peso, sexo...)
-// MÉTODOS: São Funções, servem como ações. (Ex: Cumprimentar, Codar, Apresentar-se...)
+// CLASSE: É como se fosse um molde. (Ex: Pessoa; Veiculo)
+// ATRIBUTOS: Caracteríscias. (Ex: nome, idade, peso, sexo/ carro, azul, usado, placa)
+// MÉTODOS: São Funções, servem como ações. (Ex: Cumprimentar, Codar, Apresentar-se/  Abastecer; Frear; Buzinar)
 
-public class Pessoa
+class Pessoa
 {
+    // ATRIBUTOS
+    // Ex: toda pessoa tem nome; idade; sexo ...
+    // PUBLIC significa q qualqr arquivo.cs terá acesso/visibilidade ao atributo ou método.
+    public string nome;
+    public int idade;
 
-    public string Nome{get; set;}
-    public int Idade{get; set;}
 
-
+    // MÉTODOS
+    // sempre devem iniciar com letra maíuscula.
+    // Void = Nulo (Não retorna nada)
     public void Mensagem() // Métodos sempre iniciam com letra maiuscula
     {
-        Console.WriteLine($"Prazer, me chamo {Nome} e tenho {Idade} anos.");
+        Console.WriteLine($"Prazer, me chamo {nome} e tenho {idade} anos.");
     }
 
     // public: significa q qlqr arquivo .cs terá acesso/visualização ao atributo ou método.
--- 01_Conceitos/Program.cs
+++ 01_Conceitos/Program.cs
@@ -2,23 +2,19 @@
 // using é usada p/ importar Funcionalidades; 
 // System é uma biblioteca padrão, usada para exibir e armazenar dados através do console;
 
-namespace _01_Conceitos // Local / Diretório do projeto
+namespace _01_Conceitos // Local - Diretório do projeto
 {
 
     class Program
     {
-
         static void Main(string[] args) // Todo projeto deve ter UM metodo 'static void Main'
         {
 
-            // Instanciar um objeto ; Criar algo a partir de um molde
-            Pessoa obj = new Pessoa();
-            obj.Nome = "João";
-            obj.Idade = 99;
+            // Instanciar um Objeto ; Criar algo a partir de um molde          
+            Pessoa obj = new Pessoa(); //classe nomeDoObjeto = new classe();
+            obj.nome = "João";
+            obj.idade = 99;
             obj.Mensagem();
-
-
-
         }
     }
 }
