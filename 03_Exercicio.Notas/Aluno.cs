60254ce035a5fa729dd9a8e01fe7a615235c531f
diegosfc100 <diego.sfc100@hotmail.com>
2022-03-11 23:31:58 -0300

    Update

--- 03_Exercicio.Notas/Aluno.cs
+++ 03_Exercicio.Notas/Aluno.cs
@@ -1,3 +1,5 @@
+using System; // System dá acesso p exibir msg no terminal.
+
 class Aluno
 {
     // Atributos
@@ -11,16 +13,20 @@ class Aluno
     }
 
     // Método - Situação
+    // Quando um método tem retorno, ele pode ser armazenado numa variável, 
+    // ou ser utilizado como parametro por outro método.
     public string situacao(double media)
     {
         return media >= 7 ? "APROVADO" : "Reprovado";
     }
 
     // Métodos - Mensagem
+    // Variaveis podem obter dados através de métodos, ou seja, ter métodos como valor.    
     public void mensagem()
     {
         double obterMedia = media();
         string obterSituacao = situacao(obterMedia);
+        
         Console.WriteLine($"Aluno {nome}, você está {obterSituacao} com média de {obterMedia}.");
 
     }
