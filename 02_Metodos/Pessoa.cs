d81944832d8b2d2fa580f5a215ee6f51430e7d90
diegosfc100 <diego.sfc100@hotmail.com>
2022-03-11 23:10:52 -0300

    Update

--- 02_Metodos/Pessoa.cs
+++ 02_Metodos/Pessoa.cs
@@ -2,28 +2,27 @@ using System;
 
 class Pessoa
 {
-    // Podem existir vários métodos com o mesmo nome, desde q tenham parâmetros diferentes.
-    // Método_1 / 0 parâmetro
+    // Podem existir vários MÉTODOS com o mesmo nome, desde q tenham PARÂMETROS DIFERENTES.
+    // Método01 / 0 parâmetro
 
     public void apresentar()
     {
         Console.WriteLine("Olá!");
     }
 
-
-    // Método_2 / 1 parâmetro
+    // Método02 / 1 parâmetro
     public void apresentar(string nome)
     {
         Console.WriteLine($"Olá, me chamo {nome}.");
     }
 
-
-    // Método_3 / 2 parâmetros
+    // Método03 / 2 parâmetros
     public void apresentar(string nome, int idade)
     {
         Console.WriteLine($"Olá, me chamo {nome} e tenho {idade} anos.");
     }
 
+    // Vários métodos com o mesmo nome, porém oq os diferencia são os parametros.
     
 
 }
